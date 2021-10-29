d38ca8379712b414529f7e7a93450cf4e8808532
cosmickj <kyungjun1213@gmail.com>
2021-10-29 15:42:33 +0900

    create change batch time interval page

--- frontend/src/views/AdminPage.vue
+++ frontend/src/views/AdminPage.vue
@@ -1,17 +1,100 @@
 <template>
-  <div>
-    <h1>admin</h1>
-    <div>
-      <span>Task One</span>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'Services' }">Services</router-link> | <router-link :to="{ name: 'Network ACL' }">Policies</router-link> |
+      <router-link :to="{ name: 'Event' }">Event</router-link> | <router-link :to="{ name: 'Compliances' }">Compliances</router-link> |
+      <router-link :to="{ name: 'Iam' }">IAM</router-link> |
+      <router-link :to="{ name: 'Admin' }">Admin</router-link>
     </div>
+
     <div>
-      <input type="text" />
+      <p>Security Group</p>
+      <select v-model="sgSelected">
+        <option disabled value="">Select Time Interval</option>
+        <option v-for="option in options" :value="option.value" :key="option.value">
+          {{ option.text }}
+        </option>
+      </select>
+      <button :disabled="!sgSelected" type="submit" @click="changeSgInterval(sgSelected)">Submit</button>
+    </div>
+
+    <div>
+      <p>EC2 Instance</p>
+      <select v-model="instanceSelected">
+        <option disabled value="">Select Time Interval</option>
+        <option v-for="option in options" :value="option.value" :key="option.value">
+          {{ option.text }}
+        </option>
+      </select>
+      <button :disabled="!instanceSelected" type="submit" @click="changeIntancesInterval(instanceSelected)">Submit</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      sgSelected: "",
+      instanceSelected: "",
+      options: [
+        { text: "5 m", value: 5 },
+        { text: "15 m", value: 15 },
+        { text: "30 m", value: 30 },
+        { text: "60 m", value: 60 },
+      ],
+    };
+  },
+  methods: {
+    changeSgInterval(sgSelected) {
+      axios
+        .post("http://localhost:5000/admin/taskOne", { interval: sgSelected })
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    changeIntancesInterval(instanceSelected) {
+      axios
+        .post("http://localhost:5000/admin/taskTwo", { interval: instanceSelected })
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
+}
+
+#nav a.router-link-active {
+  color: white;
+  background: crimson;
+}
+</style>
--- frontend/src/views/policies/SecurityGroup.vue
+++ frontend/src/views/policies/SecurityGroup.vue
@@ -1,9 +1,62 @@
 <template>
-  <h1>Security Group</h1>
+  <JqxGrid
+    ref="ec2grid"
+    :width="'100%'"
+    :source="dataAdapter"
+    :columns="columns"
+    :pageable="true"
+    :sortable="true"
+    :editable="true"
+    :fillterable="true"
+  />
 </template>
 
 <script>
-export default {};
+import JqxGrid from "jqwidgets-scripts/jqwidgets-vue/vue_jqxgrid.vue";
+
+export default {
+  components: {
+    JqxGrid,
+  },
+  data() {
+    return {
+      dataAdapter: new jqx.dataAdapter(this.source),
+      columns: [
+        { text: "Group Name", datafield: "GroupName", editable: false },
+        { text: "Group ID", datafield: "GroupId", editable: false },
+        { text: "VPC ID", datafield: "VpcId", editable: false },
+        { text: "Description", datafield: "Description", editable: false },
+        { text: "Comment", datafield: "InputValue", editable: true, classname: "input-column" },
+      ],
+    };
+  },
+  methods: {
+    newButton() {
+      return '<input type="button" onclick="delete(' + cellvalue + ')" value="DEL"/>';
+    },
+  },
+  beforeCreate() {
+    this.source = {
+      id: "_id",
+      url: "http://localhost:5000/security-group/sgs",
+      datatype: "json",
+      datafield: [
+        { name: "GroupName", type: "string" },
+        { name: "GroupId", type: "string" },
+        { name: "VpcId", type: "string" },
+        { name: "Description", type: "string" },
+        { name: "InputValue", type: "string" },
+      ],
+    };
+  },
+};
 </script>
 
-<style></style>
+<style>
+@import "~jqwidgets-scripts/jqwidgets/styles/jqx.base.css";
+
+.input-column {
+  background: crimson;
+  color: white;
+}
+</style>
